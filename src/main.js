8b55e10897f8247c7d7e3daad827d87d49514176
wayneliu <1005881270@qq.com>
2019-02-12 03:38:25 +0800

    部分引入

--- src/main.js
+++ src/main.js
@@ -3,12 +3,63 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
+// import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import Utils from './components/Utils.vue'
 
+// 导入必要部分
+import {
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Input,
+  Select,
+  Button,
+  Tag,
+  Alert,
+  Icon,
+  Row,
+  Col,
+  Card,
+  Carousel,
+  CarouselItem,
+  Cascader,
+  Transfer,
+  Message
+} from 'element-ui';
+
+
+
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(MenuItemGroup);
+Vue.use(Input);
+Vue.use(Select);
+Vue.use(Button);
+Vue.use(Tag);
+Vue.use(Alert);
+Vue.use(Icon);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Card);
+Vue.use(Carousel);
+Vue.use(CarouselItem);
+Vue.use(Cascader);
+Vue.use(Transfer);
+Vue.prototype.$message = Message;
+
+
+
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
 Vue.prototype.Utils = Utils;
 /* eslint-disable no-new */
 new Vue({
