3fd3f940af5ff5f2982af3b7d6eb7eec46c77b0e
wayneliu <1005881270@qq.com>
2019-02-12 01:01:08 +0800

    css

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div style="width: 50%;margin: 20px auto;display: flex">
-      <el-tag class="font_2 subtitle" style="width: 145px;" >产品系列</el-tag>
-        <div style="border: 1px  solid #441C08;margin: 80px 20px 20px 20px;width: 250px;border-radius:5px;" v-for="item in mainClass" @click="goSerial(item)">
-          <p class="font_7" style="font-size: 15px;margin: 10px 0 0 10px">{{item.nameCN}}</p>
-          <p class="font_7" style="font-size: 15px;margin: 10px 0 0 10px">{{item.nameEN}}</p>
+      <el-tag class="font_2 subtitle img-border bord-rad" style="width: 145px;" >产品系列</el-tag>
+        <div class="img-border bord-rad" style="margin: 80px 20px 20px 20px;width: 250px;" v-for="item in mainClass" @click="goSerial(item)">
+          <p class="font_7 item-p">{{item.nameCN}}</p>
+          <p class="font_7 item-p">{{item.nameEN}}</p>
           <hr>
           <div style="margin-top: -15px">
             <span class="font_4" style="margin: 0 10px 20px 10px;">
@@ -14,11 +14,11 @@
         </div>
     </div>
     <div style="width: 50%;margin: 30px auto;">
-      <p style="text-indent:2em;font-size: 16px;line-height: 1.8em" class="font_6">安腾达化工与全球知名化工企业紧密合作，致力于引进国外先进的化工材料，发掘国内优秀材料，为广大客户提供优质产品与服务。
+      <p style="text-indent:2em;" class="font_6 font_6-size">安腾达化工与全球知名化工企业紧密合作，致力于引进国外先进的化工材料，发掘国内优秀材料，为广大客户提供优质产品与服务。
         公司拥有多名从事橡塑行业十余年的技术工程师和技术顾问，可为客人提供专业的技术服务和解决方案，并可以提供定制化的产品。
       </p>
-      <p style="font-size: 16px;line-height: 1.8em" class="font_6">服务范围:</p>
-      <ol style="font-size: 16px;line-height: 1.8em" class="font_6">
+      <p  class="font_6 font_6-size">服务范围:</p>
+      <ol  class="font_6 font_6-size">
         <li>提供优质化工产品及使用指导服务。</li>
         <li>提供产品解决方案和技术指导服务。</li>
         <li>提供材料科学研究、市场信息、投资咨询、技术开发、新材料技术转让服务。</li>
@@ -26,20 +26,20 @@
     </div>
 
     <div style="width: 50%;height:435px;margin: 50px auto;">
-      <el-tag class="font_2 subtitle" style="width: 145px;">推荐产品</el-tag>
+      <el-tag class="font_2 subtitle img-border bord-rad" style="width: 145px;">推荐产品</el-tag>
       <el-carousel :interval="4000" height="400px" style="margin-top: 20px" >
         <el-carousel-item v-for="pro in productImg" >
           <router-link :to="{name:'Detail',params:{'nameCN':pro.name}}">
             <img style="width: 659px;height: 310px;margin: 0 150px;" class="img-shadow2"  :src="pro.url" alt="">
-            <p style="margin: 0 150px;font-size: 18px;" class="font_6">{{pro.name}}</p>
-            <p style="margin: 0 150px;font-size:13px;" class="font_10" >{{pro.spec1}}</p>
-            <p style="margin: 0 150px;font-size:13px;" class="font_10" >{{pro.spec2}}</p>
+            <p class="font_6 pro-p-margin pro-name-size">{{pro.name}}</p>
+            <p class="font_10 pro-p-margin pro-size" >{{pro.spec1}}</p>
+            <p class="font_10 pro-p-margin pro-size" >{{pro.spec2}}</p>
           </router-link>
         </el-carousel-item>
       </el-carousel>
     </div>
     <div style="width: 50%;height:90px;margin: 20px auto;">
-      <el-tag class="font_2 subtitle" style="width: 205px;">产品目录下载</el-tag>
+      <el-tag class="font_2 subtitle img-border bord-rad" style="width: 205px;">产品目录下载</el-tag>
       <a href="../../static/files/dc2.pdf" download style="margin: 20px 0">
         <img src="../../static/imgs/products/pdflogo.png" alt=""  style="display: block;margin: 20px 0 0 0 ">
         ATT产品目录2015.pdf</a>
@@ -108,12 +108,19 @@
 </script>
 
 <style scoped>
+  .pro-name-size{
+    font-size: 18px;
+  }
+  .pro-size{
+    font-size: 13px;
+  }
+  .pro-p-margin{
+    margin: 0 150px;
+  }
   .subtitle{
     font-size: 30px;
     color: #441C08;
-    border: 1px solid #441C08;
     height: 45px;
-    border-radius:5px;
     background: white;
     display: block;
     line-height:45px;
@@ -122,5 +129,15 @@
   .img-shadow2{
     border:2px solid #441C08;
     box-shadow: 0 3px 4px rgba(0,0,0,0.27)
+  }
+
+
+  .font_6-size{
+    font-size: 16px;
+    line-height: 1.8em;
+  }
+  .item-p{
+    font-size: 15px;
+    margin: 10px 0 0 10px;
   }
 </style>
